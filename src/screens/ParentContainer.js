6094b4deafb99b53edb8fa61c8a0a5ae8f9faec8
tiongchuan <tiongchuan.ongtc@gmail.com>
2022-06-05 23:30:48 +0800

    test deployment

---
+++ src/screens/ParentContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {useState, useEffect} from "react";
+import APT from "../API";
+
+const ParentContainer = () => {
+
+    return (
+        <>
+        Parent Container
+        </>
+    );
+}
+
+export default ParentContainer;
