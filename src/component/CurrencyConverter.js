c8c504bf39e96b9f3eab2714e9922298933ffb85
MiaKim0804 <kimmichino0804@gmail.com>
2022-10-09 18:55:30 +0800

    update:code review

--- src/component/CurrencyConverter.js
+++ src/component/CurrencyConverter.js
@@ -2,95 +2,108 @@ import React, { useEffect, useState } from "react";
 import CurrencyData from "./CurrencyData";
 import API from "../API";
 import './CurrencyConverter.css'
+import sgd from './img/sgd.jpg'
 
 const CurrencyConverter = () => {
 
-    const [input, setInput] = useState('SGD');
-    const [output, setOutput] = useState('USD');
-    const [amount, setAmount] = useState(1);
-    const [latestRate, setLatestRate] = useState([])
-    const [amountConverted, setAmountConverted] = useState('');
-    const [conversionRate, setConversionRate] = useState('');
+  const [changeFrom, setChangeFrom] = useState('SGD');
+  const [changeTo, setChangeTo] = useState('USD');
+  const [amount, setAmount] = useState(1);
+  const [currencyData, setCurrencyData] = useState([])
+  const [amountConverted, setAmountConverted] = useState('');
+  const [latestRate, setLatestRate] = useState('');
 
-    let date = new Date()
-    let showDate = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`
+  // Get today's date
+  let date = new Date()
+  let showDate = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`
+  
+  // Set amount 
+  const handleAmount = (e) => {
+      setAmount(e.target.value)
+  }
 
-    const handleInput = (e) => {
-        setInput(e.target.value)
-    }
-    const handleOutput = (e) => {
-        setOutput(e.target.value)
-    }                    
-    const handleAmount = (e) => {
-        setAmount(e.target.value)
-    }
+  // Set change from 
+  const handleChangeFrom = (e) => {
+      setChangeFrom(e.target.value)
+  }
 
-    const GetLatestRate = async () => {
-        const { request, data } = await API.get(`/latest`)
-        if (request.status === 200) {
-            // console.log(data);
-            const changeCurrencyArray = Object.keys(data.rates)
-            // console.log(changeCurrencyArray);
-            setLatestRate(changeCurrencyArray);
-            // console.log(latestRate);
-        }
-    }
+  // Set change to 
+  const handleChangeTo = (e) => {
+      setChangeTo(e.target.value)
+  }                    
 
-    useEffect(() => {
-        GetLatestRate()
-    }, [])
+  // Get currencies 
+  const getCurrencyData = async () => {
+      const { request, data } = await API.get(`/latest`)
+      if (request.status === 200) {
+          // console.log(data);
+          const currencyData = Object.keys(data.rates)
+          // console.log(changeCurrencyArray);
+          setCurrencyData(currencyData);
+          // console.log(latestRate);
+      }
+  }
 
-    const ConvertCurrency = async () => {
-        const {request, data} = await API.get(`/convert?from=${input}&to=${output}&amount=${amount}`)
-        if (request.status === 200) {
-            // console.log(res);
-            setAmountConverted((data.result).toFixed(3));
-            setConversionRate((data.info.rate).toFixed(3));
-        }
-    }
+  useEffect(() => {
+      getCurrencyData()
+  }, [])
 
-    useEffect(()=>{
-        ConvertCurrency()
-    },[handleInput, handleOutput])
+  // Convert currency 
+  const ConvertCurrency = async () => {
+      const {request, data} = await API.get(`/convert?from=${changeFrom}&to=${changeTo}&amount=${amount}`)
+      if (request.status === 200) {
+          // console.log(res);
+          setAmountConverted((data.result).toFixed(2));
+          setLatestRate((data.info.rate).toFixed(2));
+      }
+  }
 
-    return (
-        <>
-            <div className="background"></div>
-            <div className="container">
-                <div className="header">
-                    <h2>Go Currency</h2>
-                    <p>{showDate}</p>
-                </div>
-              
-                <div className="form">
-                    <input 
-                        type='number'
-                        value={amount} onChange={handleAmount}>
-                    </input>
-                    <select 
-                        type = 'text' 
-                        value={input} onChange={handleInput}>
-                        {latestRate.map((o) => 
-                            <option key={o}>{o}</option>
-                        )}
-                     </select>
-                    <select 
-                        type = 'text'
-                        value={output} 
-                        onChange={handleOutput}>
-                        {latestRate.map((o) =>
-                            <option key={o}>{o}</option>
-                        )}
-                    </select>
-                </div>
-                <div className="output_field">
-                    <h5>{amount} {input} = {amountConverted} {output}</h5>
-                    <p>1 {input} = {conversionRate} {output}</p>
-                </div>
-            <CurrencyData input={input} output={output}/>
+  useEffect(()=>{
+    ConvertCurrency()
+  },[handleChangeFrom, handleChangeTo, handleAmount])
+
+  return (
+    <>
+      <img src={sgd} className="background1" />
+      <div className="background2">
+        <div className="container">
+          <div className="leftContainer">
+            <div className="header">
+              <h2>Go Currency</h2>
+              <p>{showDate}</p>
             </div>
-        </>
-    )
+            <div className="form">
+              <input 
+                  type='number'
+                  value={amount} onChange={handleAmount}>
+              </input>
+              <div className="select">
+                <select 
+                  type = 'text' 
+                  value={changeFrom} onChange={handleChangeFrom}>
+                  {currencyData.map((o) => 
+                    <option key={o}>{o}</option>
+                  )}
+                </select>
+                <select 
+                  type = 'text'
+                  value={changeTo} onChange={handleChangeTo}>
+                  {currencyData.map((o) =>
+                    <option key={o}>{o}</option>
+                  )}
+                </select>
+              </div>
+            </div>
+            <div className="output_field">
+                <h5>{amount} {changeFrom} = {amountConverted} {changeTo}</h5>
+                <p>1 {changeFrom} = {latestRate} {changeTo}</p>
+            </div>
+          </div>
+          <CurrencyData changeFrom={changeFrom} changeTo={changeTo} />
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default CurrencyConverter;
--- src/component/CurrencyData.js
+++ src/component/CurrencyData.js
@@ -6,109 +6,128 @@ import './CurrencyData.css'
 
 const CurrencyData = (props) => {
 
-    const {input, output} = props
+  // Pass in props from CurrencyConverter.js
+  const {changeFrom, changeTo} = props
 
-    const [data1, setData1] = useState([])
-    const [chartData, setChartData] =
-        useState({
-            labels: '',
-            datasets: [{
-                label: 'currency rates',
-                data: ''
-            }]
-        })
+  const [data1, setData1] = useState([])
+  const [chartData, setChartData] =
+    useState({
+      labels: '',
+      datasets: [{
+        label: 'currency rates',
+        data: ''
+      }]
+    })
 
-    const d1 = new Date();
-    const todayDate = d1.toISOString().slice(0, 10);
-    const d1Number = d1.getTime();
-    const d5Number = d1Number - (4 * 24 * 60 * 60 * 1000);
-    const d5 = new Date(d5Number)
-    const startDate = d5.toISOString().slice(0, 10);
+  // Get start date and end date
+  let date = new Date()
+  let endDate = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`  
+  let startDate = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()-6}`
 
-    const getCurrencyData = async () => {
-        const { data } = await API.get(`/timeseries?start_date=${startDate}&end_date=${todayDate}&base=${input}`)
-        setData1(data.rates)
-        // console.log(data);
-    }
+  const getCurrencyData = async () => {
+    const { data } = await API.get(`/timeseries?start_date=${startDate}&end_date=${endDate}&base=${changeFrom}&symbols=${changeTo}`)
+    setData1(data.rates)
+    // console.log(data);
+  }
 
-    useEffect(() => {
-        getCurrencyData()
-    }, [input, output])
+  useEffect(() => {
+    getCurrencyData()
+  }, [changeFrom, changeTo])
 
-    const function1 = () => {
-        const label5Days = (Object.keys(data1));
-        // console.log(label5Days);
-        let array5Days = []
-        const array = [data1]
-        const result = array.flatMap(Object.values);
-        // console.log(result);
-        for (let i = 0; i < result.length; i++) {
-            const result1 = Object.entries(result[i])
-            // console.log(result1);
-            for (let k = 0; k < result1.length; k++) {
-                if (result1[k][0] === output) {
-                    array5Days.push(result1[k][1]);
+  // Get labels and data for chart  
+  const getChartData = () => {
+    let labels = Object.keys(data1)  // Get dates
+    let data = Object.values(data1)  // Get rates
+    let data2 = data.map((item) => {  // Get rates for selected currency
+      return item[changeTo]
+    })
+    setChartData({  
+      labels: labels,
+      datasets: [{
+        label: `${changeFrom} to ${changeTo}`,
+        data: data2,
+        fill: true,
+        backgroundColor:'#ffffff29' 
+      }]
+    })
+  }
+
+  useEffect(() => {
+    getChartData()
+  }, [data1])
+
+  return (
+    <>
+      <div>
+        <Line  
+          className="chart"
+          style={{display: changeFrom? true:'none'}}
+          data={chartData}
+          options={{
+            // Change the color of x-axis and y-axis
+            scales: {
+              x: {
+                ticks: {
+                  color: '#ffffff',
+                  font: {
+                    size: 9
+                  }
                 }
+              },
+              y: {
+                ticks: {
+                  color: '#ffffff',
+                  font: {
+                    size: 9
+                  }
+                }
+              }
+            },
+
+            // Set border color of the chart area 
+            elements: {
+              line: {
+                borderColor: '#ffffff',
+                borderWidth: 1
+              },
+              point: {
+                radius: 1,
+                borderColor: '#ffffff',
+              }
+            },
+            
+            // Set title of the chart
+            plugins: {
+              title: {
+                display: true,
+                text: 'Currency rate for past 7 days',
+                color: '#ffffff',
+                font: {
+                  size: 13
+                }
+              },
+
+              // Change the color of the label
+              legend: {
+                display: true,
+                position: 'top',
+                labels: {
+                  color: '#ffffff',
+                  font: {
+                    size: 10
+                  }
+                }
+              }
             }
-        }
-        // console.log(array5Days);
+          }} 
 
-        setChartData({
-            labels: label5Days,
-            datasets: [{
-                label: 'currency rates',
-                data: array5Days,
-                tension:0.4,
-                fill: true,
-                backgroundColor:'#ffffff29',
-                pointBackgroundColor:'#ffffffdb',
-                borderColor: '#ffffff45',
-                segment: {
-                    borderWidth: '2px',
-                    borderColor: '#ffffff',
-                }
-            }]  
-        })
-        console.log(chartData);
-    }
-    
-    useEffect(() => {
-        function1()
-    }, [data1])
-
-    return (
-        <>
-            <div className="chart">
-                <p className="p">{input} vs {output}</p>
-                <Line  
-                    style={{display: output? true:'none'}}
-                    data={chartData}
-                    options={{
-                        plugins: {
-                            title: {
-                                display: true,
-                                text: 'Currency rate for past 5 days',
-                                fontSize:2,
-                                color: '#ffffff99'
-                            },
-                            legend: {
-                                labels: {
-                                    label: {
-                                        font: {
-                                        size:10,
-                                        color:'white' 
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }} 
-                    height = '200px'
-                    width= '300px'
-                />
-            </div>
-        </>
-    )
+          // Set the height and the width of the chart 
+          height={333}
+          width={420}
+        />
+      </div>
+    </>
+  )
 }
 
 export default CurrencyData;
